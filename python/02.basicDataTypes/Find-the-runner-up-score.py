9c11bad930796e63f71d000e8da896d9de49ded5
Mohamed Ali <60547028+moli-debugger@users.noreply.github.com>
2021-10-03 23:56:55 +0530

    Delete

--- python/02.basicDataTypes/Find-the-runner-up-score.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    list=[]
-    for x in arr:
-        if x not in list:
-            list.append(x)
-    list.sort()
-    print(list[-2])
