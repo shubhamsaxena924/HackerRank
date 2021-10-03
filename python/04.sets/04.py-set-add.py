9a8c9d7a5227159bc405e54054ed22da2dae7cee
divyansh singh <52194749+devsingh1234@users.noreply.github.com>
2021-10-03 22:27:51 +0530

    added problems to python/sets

---
+++ python/04.sets/04.py-set-add.py
@@ -0,0 +1,6 @@
+n=int(input())
+a=set()
+for i in range(n-1):
+    a.add(input())
+print(len(a))
+
---
+++ python/04.sets/08.py-set-difference-operation.py
@@ -0,0 +1,5 @@
+n=input()
+N=set(input().split())
+b=input()
+B=set(input().split())
+print(len(N.difference(B)))
